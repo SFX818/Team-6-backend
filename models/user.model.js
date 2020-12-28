899e6f90769aa1e42fcf0275807c5b77a129cba3
cam-em <cmoreno.1117@gmail.com>
2020-12-28 16:37:44 -0600

    updated user and location model to include zipcode and county

--- models/user.model.js
+++ models/user.model.js
@@ -6,6 +6,8 @@ const User = mongoose.model(
         username:String,
         email:String,
         password:String,
+        zipcode:String,
+        county: String,
         roles: [
             {
                 type: mongoose.Schema.Types.ObjectId,
