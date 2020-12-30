a022de2636a9f59b27ce5f146d8f0ebffa944aa1
dazmag <iscope09@hotmail.com>
2020-12-29 20:51:24 -0500

    add route

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -9,23 +9,18 @@ module.exports = app => {
     router.post("/", location.findOrCreate)
 
     // Retrieve all Locations
-    // router.get("/", location.findAll);
+    router.get("/", location.findAll);
 
     // Retrieve a single Location with id
     router.get("/:id", location.findOne);
 
-    // Add to favorite locations
-    router.get("/favorite/:id", [authJwt.verifyWebToken],location.addToFavoriteLocations);
+ //Update a location with id
+    router.put("/:id", location.update);
 
-    // Retrieve all search locations
-    router.get("/history/:id", [authJwt.verifyWebToken], location.addToSearchLocations);
-
+    // Delete a Location with id
+    router.delete("/:id", location.delete);
     // Api
     app.use('/api/location', router)
 }
 
-    // Update a location with id
-    // router.put("/:id", location.update);
-
-    // Delete a Location with id
-    // router.delete("/:id", location.delete);
+   
